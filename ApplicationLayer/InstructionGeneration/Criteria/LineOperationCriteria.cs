22a7be1d8fe9996989a623355c4ebcda9a52e953
Dmitrii <Dmitrii@DMITRII>
2026-01-10 06:09:03 +0700

    Руссификация LineOperationCriteria.cs

--- ApplicationLayer/InstructionGeneration/Criteria/LineOperationCriteria.cs
+++ ApplicationLayer/InstructionGeneration/Criteria/LineOperationCriteria.cs
@@ -3,17 +3,17 @@ using ApplicationLayer.InstructionGeneration.Models;
 namespace ApplicationLayer.InstructionGeneration.Criteria;
 
 /// <summary>
-/// Defines criteria for a single line operation for a specific device and side.
+/// Определяет критерии для одной операции линии для конкретного устройства и стороны.
 /// </summary>
 public sealed record LineOperationCriteria
 {
     /// <summary>
-    /// Initializes a new instance of the <see cref="LineOperationCriteria"/> class.
+    /// Инициализирует новый экземпляр класса <see cref="LineOperationCriteria"/>.
     /// </summary>
-    /// <param name="lineCode">The line identifier.</param>
-    /// <param name="side">The side of the line.</param>
-    /// <param name="deviceObjectId">The device object identifier.</param>
-    /// <param name="actionCode">The action code for the operation.</param>
+    /// <param name="lineCode">Идентификатор линии.</param>
+    /// <param name="side">Сторона линии.</param>
+    /// <param name="deviceObjectId">Идентификатор объекта устройства.</param>
+    /// <param name="actionCode">Код действия для операции.</param>
     public LineOperationCriteria(string lineCode, SideOfLine side, int deviceObjectId, ActionCode actionCode)
     {
         LineCode = lineCode;
@@ -23,137 +23,137 @@ public sealed record LineOperationCriteria
     }
 
     /// <summary>
-    /// Gets the line identifier.
+    /// Получает идентификатор линии.
     /// </summary>
     public string LineCode { get; init; }
 
     /// <summary>
-    /// Gets the side of the line.
+    /// Получает сторону линии.
     /// </summary>
     public SideOfLine Side { get; init; }
 
     /// <summary>
-    /// Gets the device object identifier.
+    /// Получает идентификатор объекта устройства.
     /// </summary>
     public int DeviceObjectId { get; init; }
 
     /// <summary>
-    /// Gets the optional device name.
+    /// Получает необязательное имя устройства.
     /// </summary>
     public string? DeviceName { get; init; }
 
     /// <summary>
-    /// Gets the action code.
+    /// Получает код действия.
     /// </summary>
     public ActionCode ActionCode { get; init; }
 
     /// <summary>
-    /// Gets a value indicating whether the device has DFZ.
+    /// Получает значение, указывающее, есть ли у устройства DFZ.
     /// </summary>
     public bool HasDFZ { get; init; }
 
     /// <summary>
-    /// Gets a value indicating whether DFZ is enabled.
+    /// Получает значение, указывающее, включен ли DFZ.
     /// </summary>
     public bool DFZEnabled { get; init; }
 
     /// <summary>
-    /// Gets a value indicating whether DFZ is connected to line CT.
+    /// Получает значение, указывающее, подключен ли DFZ к трансформатору тока линии.
     /// </summary>
     public bool DFZConnectedToLineCT { get; init; }
 
     /// <summary>
-    /// Gets a value indicating whether DFZ is connected to line VT.
+    /// Получает значение, указывающее, подключен ли DFZ к трансформатору напряжения линии.
     /// </summary>
     public bool DFZConnectedToLineVT { get; init; }
 
     /// <summary>
-    /// Gets a value indicating whether line VT should be switched to reserve.
+    /// Получает значение, указывающее, нужно ли переключить трансформатор напряжения линии на резерв.
     /// </summary>
     public bool NeedSwitchLineVTToReserve { get; init; }
 
     /// <summary>
-    /// Gets a value indicating whether bus VT should be switched to reserve.
+    /// Получает значение, указывающее, нужно ли переключить трансформатор напряжения шины на резерв.
     /// </summary>
     public bool NeedSwitchBusVTToReserve { get; init; }
 
     /// <summary>
-    /// Gets a value indicating whether both CB CTs are on the reverse side of the line.
+    /// Получает значение, указывающее, находятся ли оба трансформатора тока выключателя на обратной стороне линии.
     /// </summary>
     public bool BothCbctReverseSide { get; init; }
 
     /// <summary>
-    /// Gets a value indicating whether the function is the only one in the device.
+    /// Получает значение, указывающее, является ли функция единственной в устройстве.
     /// </summary>
     public bool IsOnlyFunctionInDevice { get; init; }
 
     /// <summary>
-    /// Gets a value indicating whether the device has DZL.
+    /// Получает значение, указывающее, есть ли у устройства DZL.
     /// </summary>
     public bool HasDZL { get; init; }
 
     /// <summary>
-    /// Gets a value indicating whether DZL is enabled.
+    /// Получает значение, указывающее, включен ли DZL.
     /// </summary>
     public bool DZLEnabled { get; init; }
 
     /// <summary>
-    /// Gets a value indicating whether the device has DZ.
+    /// Получает значение, указывающее, есть ли у устройства DZ.
     /// </summary>
     public bool HasDZ { get; init; }
 
     /// <summary>
-    /// Gets a value indicating whether DZ is enabled.
+    /// Получает значение, указывающее, включен ли DZ.
     /// </summary>
     public bool DZEnabled { get; init; }
 
     /// <summary>
-    /// Gets a value indicating whether DZ is connected to line VT.
+    /// Получает значение, указывающее, подключен ли DZ к трансформатору напряжения линии.
     /// </summary>
     public bool DZConnectedToLineVT { get; init; }
 
     /// <summary>
-    /// Gets a value indicating whether the device has OAPV.
+    /// Получает значение, указывающее, есть ли у устройства OAPV.
     /// </summary>
     public bool HasOAPV { get; init; }
 
     /// <summary>
-    /// Gets a value indicating whether OAPV is enabled.
+    /// Получает значение, указывающее, включен ли OAPV.
     /// </summary>
     public bool OAPVEnabled { get; init; }
 
     /// <summary>
-    /// Gets a value indicating whether the device has TAPV.
+    /// Получает значение, указывающее, есть ли у устройства TAPV.
     /// </summary>
     public bool HasTAPV { get; init; }
 
     /// <summary>
-    /// Gets a value indicating whether TAPV is enabled.
+    /// Получает значение, указывающее, включен ли TAPV.
     /// </summary>
     public bool TAPVEnabled { get; init; }
 
     /// <summary>
-    /// Gets a value indicating whether UPASK receivers should be disabled.
+    /// Получает значение, указывающее, должны ли быть отключены приемники UPASK.
     /// </summary>
     public bool NeedDisableUpaskReceivers { get; init; }
 
     /// <summary>
-    /// Gets a value indicating whether line CT should be disconnected from DZO.
+    /// Получает значение, указывающее, должен ли трансформатор тока линии быть отключен от DZO.
     /// </summary>
     public bool NeedDisconnectLineCTFromDZO { get; init; }
 
     /// <summary>
-    /// Gets a value indicating whether MTZO shinovka A to B is required.
+    /// Получает значение, указывающее, требуется ли переключение MTZO шиновки с A на B.
     /// </summary>
     public bool NeedMtzoShinovkaAtoB { get; init; }
 
     /// <summary>
-    /// Gets a value indicating whether the CT remains energized on this side.
+    /// Получает значение, указывающее, остается ли трансформатор тока под напряжением на этой стороне.
     /// </summary>
     public bool CtRemainsEnergizedOnThisSide { get; init; }
 
     /// <summary>
-    /// Gets a value indicating whether field closing is allowed.
+    /// Получает значение, указывающее, разрешено ли включение поля.
     /// </summary>
     public bool IsFieldClosingAllowed { get; init; }
 }
